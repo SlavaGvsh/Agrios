5e123cd785f0ada7e8871e7c4007257f8615711a
Slava.Gvsh <slava.gvsh@gmail.com>
2024-05-09 15:16:49 +0300

    add section projects

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// $(document).ready(function() {
+//     $('.burger-menu, .burger-menu-close').click(function(event) {
+//        $('.nav,.burger-menu-close, .burger-menu').toggleClass('active');
+//        $('body').toggleClass('lock');
+
+//     });
+//     //  tabs
+//   $(".contact__item > a").click(function (e) {
+//    e.preventDefault();
+
+//    var currentAttrValue = $(this).attr("href");
+
+//    console.log(currentAttrValue);
+
+//    // Показываем содержимое вкладки
+//    $(".contact__body").removeClass("active");
+//    $(currentAttrValue).addClass("active");
+
+//    // Делаем текущую вкладку активной
+//    $(".contact__item > a").removeClass("active");
+//    $(this).addClass("active");
+//  });
+// });
+
+$(document).ready(function () {
+  $(".burger-menu, .burger-menu-close").click(function (event) {
+    $(".nav,.burger-menu-close, .burger-menu").toggleClass("active");
+    //  $('body').toggleClass('lock');
+  });
+  showDrop();
+
+  $(window).resize(function () {
+    showDrop();
+  });
+
+  //   $(document).click(function (event) {
+  //     if (!$(event.target).closest(".nav__list").length) {
+  //       // Если клик произошел не внутри .nav__menu, скрываем все открытые дропдауны
+  //       $("a.drop-btn").removeClass("active");
+  //     }
+  //   });
+});
+
+function showDrop() {
+  var screenWidth = $(window).width();
+  console.log(screenWidth);
+  if (screenWidth < 768) {
+    $(".nav__link").click(function (event) {
+      $(".nav__item").not($(this).parent()).removeClass("active");
+      $(this).parent(".nav__item").toggleClass("active");
+
+      event.stopPropagation();
+    });
+  } else {
+    $(".nav__item").removeClass("active");
+  }
+}
+
+$(".owl-carousel").owlCarousel({
+  items: 1,
+  // margin: 10,
+  autoHeight: true,
+  dots: false,
+  nav: true,
+  navText: false,
+  autoplaySpeed: 1000,
+  autoplay: true,
+  slideSpeed: 2000,
+});
